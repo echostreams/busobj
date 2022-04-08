b0449b6352c459cdeabe95a6613b0517b2dca2c1
Steven Deng <steven.deng@premioinc.com>
2022-04-08 08:49:31 -0700

    Add bus-server

--- busobj/windows/getuid.cpp
+++ busobj/windows/getuid.cpp
@@ -161,6 +161,34 @@ extern "C" {
         return ret;
     }
 
+    char*
+    secure_getenv(char const* name)
+    {
+#if HAVE___SECURE_GETENV /* glibc */
+        return __secure_getenv(name);
+#elif HAVE_ISSETUGID /* OS X, FreeBSD, NetBSD, OpenBSD */
+        if (issetugid())
+            return NULL;
+        return getenv(name);
+#elif HAVE_GETUID && HAVE_GETEUID && HAVE_GETGID && HAVE_GETEGID /* other Unix */
+        if (geteuid() != getuid() || getegid() != getgid())
+            return NULL;
+        return getenv(name);
+#elif defined _WIN32 && ! defined __CYGWIN__ /* native Windows */
+        /* On native Windows, there is no such concept as setuid or setgid binaries.
+           - Programs launched as system services have high privileges, but they don't
+             inherit environment variables from a user.
+           - Programs launched by a user with "Run as Administrator" have high
+             privileges and use the environment variables, but the user has been asked
+             whether he agrees.
+           - Programs launched by a user without "Run as Administrator" cannot gain
+             high privileges, therefore there is no risk. */
+        return getenv(name);
+#else
+        return NULL;
+#endif
+    }
+
 #ifdef __cplusplus
 }
 #endif
@@ -177,5 +205,7 @@ int main()
     return EXIT_SUCCESS;
 }
 #endif
+
+
 
 #endif // WIN32 || WIN64
